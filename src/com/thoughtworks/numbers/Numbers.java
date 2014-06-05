3123c0438498f31488197e7c51cf4220d1f26cb4
amber <amber.houle3@gmail.com>
2014-06-05 15:35:52 -0500

    computes the square root of numbers larger than 4

--- src/com/thoughtworks/numbers/Numbers.java
+++ src/com/thoughtworks/numbers/Numbers.java
@@ -19,6 +19,39 @@ public class Numbers {
         List<Integer> numbers = new ArrayList<Integer>();
         addAll(numbers, source);
 
+        printSquareRoots(squareRoot(numbersLargerThanFour(numbers)));
+    }
+
+    private static void printSquareRoots(List<Double> squareRoots){
+        System.out.println(join(squareRoots));
+    }
+
+    private static List<Double> squareRoot(List<Integer> numbers){
+        List<Double> squareRoots = new ArrayList<Double>();
+        for (Integer number : numbers) {
+            squareRoots.add(Math.sqrt(number));
+        }
+        return squareRoots;
+    }
+
+    private static List<Integer> numbersLargerThanFour(List<Integer> numbers){
+        List<Integer> largerThanFour = new ArrayList<Integer>();
+        for (Integer number : numbers) {
+            if(number > 4){
+                largerThanFour.add(number);
+            }
+        }
+        return largerThanFour;
+    }
+
+    private static String join(List<Double> listItems){
+        String joinedString = "";
+
+        for (Double listItem : listItems) {
+            joinedString += listItem + ", ";
+        }
+        joinedString = joinedString.substring(0, joinedString.length()-2);
+        return joinedString;
     }
 
 }
